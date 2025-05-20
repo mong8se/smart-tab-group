490e548ed2c459e2a29d5c3ced95615ac3438aaf
Steven Moazami <mong8se@users.noreply.github.com>
2025-05-20 16:09:08 -0400

    Add script to build manifest from common base

---
+++ scripts/build.ts
@@ -0,0 +1,29 @@
+import { merge } from "lodash";
+
+async function main() {
+  const [base, chrome, firefox] = await Promise.all([
+    Deno.readTextFile("src/manifest.json").then((data) => JSON.parse(data)),
+    Deno.readTextFile("src/chrome/manifest.json").then((data) =>
+      JSON.parse(data),
+    ),
+    Deno.readTextFile("src/firefox/manifest.json").then((data) =>
+      JSON.parse(data),
+    ),
+  ]);
+
+  const chromeManifest = merge({}, base, chrome);
+  const firefoxManifest = merge({}, base, firefox);
+
+  await Promise.all([
+    Deno.writeTextFile(
+      "build/chrome/manifest.json",
+      JSON.stringify(chromeManifest, null, 2),
+    ),
+    Deno.writeTextFile(
+      "build/firefox/manifest.json",
+      JSON.stringify(firefoxManifest, null, 2),
+    ),
+  ]);
+}
+
+await main();
